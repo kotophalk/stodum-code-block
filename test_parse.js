a8630770159aba211056fcc152c06844b778ca0c
kotophalk <kotophalk@gmail.com>
2026-04-12 07:21:46 +0300

    chore: remove test script

--- test_parse.js
+++
@@ -1,7 +0,0 @@
-// Mocking the block comment structure
-const text = '<!-- wp:stodum/code-block {"language":"bash","content":"echo hello"} -->\n<div class="stodum-code-wrapper"></div>\n<!-- /wp:stodum/code-block -->';
-const match = text.match(/<!-- wp:stodum\/code-block (\{.*\}) -->/);
-if (match) {
-    const attrs = JSON.parse(match[1]);
-    console.log("Parsed Attrs:", attrs);
-}
